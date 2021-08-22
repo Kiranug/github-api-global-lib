706dbd9c5675fc5e515115efb96a2939b5794652
Kiranug <kiran.ug123@gmail.com>
2021-08-22 17:33:01 +0530

    Update lazyDocker.groovy

--- vars/lazyDocker.groovy
+++ vars/lazyDocker.groovy
@@ -2,6 +2,7 @@ def call(Map config) {
   def containerId = (env.JOB_NAME+env.BUILD_NUMBER).toLowerCase()
   def workspace = env.WORKSPACE
   sh """
+    ls
     docker build --tag ${config.imageName} --build-arg FORCE_TESTS=`date +%s` --target testing .
     docker run --name ${containerId} -t -d -u 1000:1000 -w ${workspace} -v ${workspace}:${workspace}:rw,z -v ${workspace}@tmp:${workspace}@tmp:rw,z --entrypoint cat ${config.imageName}
     docker cp ${containerId}:${workspace}/tests build/tests/
