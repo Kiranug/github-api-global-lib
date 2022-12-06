3c7b52167f1be90327c8a28362285aa74d1230f5
Kiranug <kiran.ug123@gmail.com>
2022-12-06 18:26:50 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,1 +1,47 @@
 
+@Library ('My-Shared-Library@main') _
+
+import groovy.json.*
+import groovy.json.JsonSlurperClassic
+import net.sf.json.groovy.JsonSlurper
+import groovy.json.JsonOutput
+import groovy.json.JsonBuilder
+import groovy.io.FileType
+import hudson.FilePath;
+import jenkins.model.Jenkins;
+def call(body) 
+{	
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+pipeline {
+    agent any 
+    stages {
+        stage ('Hello World') {
+        steps {
+        helloWorld()
+        }
+     }
+       stage ('Read Yaml') {
+        steps {
+            script {
+                   def config_json=libraryResource "appconfig/deveast.json"
+	               def ConfigInputJSON = new JsonSlurperClassic().parseText(config_json)
+				project_id = ConfigInputJSON."${Environment_Name}"."#PROJECT_ID#";
+				deployment_credential_id = ConfigInputJSON."${Environment_Name}"."deployment_credential_id";
+				build_credential_id = ConfigInputJSON."${Environment_Name}"."build_credential_id";
+				cluster_name = ConfigInputJSON."${Environment_Name}"."cluster_name";
+				cluster_region = ConfigInputJSON."${Environment_Name}"."cluster_region";
+				jump_iap_server = ConfigInputJSON."${Environment_Name}"."jump_iap_server";
+				jump_server_region = ConfigInputJSON."${Environment_Name}"."jump_server_region";
+                println project_id
+            	println deployment_credential_id
+            	println cluster_name
+            }
+        }
+     }
+    }
+}
+}
