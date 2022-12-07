ad9ed0285ff85fdc8d1f655407de5bc0289a6195
Kiranug <kiran.ug123@gmail.com>
2022-12-07 16:38:11 +0530

    Update testfunction.groovy

--- vars/testfunction.groovy
+++ vars/testfunction.groovy
@@ -6,11 +6,15 @@ import groovy.json.JsonBuilder
 import groovy.io.FileType
 import hudson.FilePath;
 import jenkins.model.Jenkins;
-
-
-def call(int a, int b) {
+def call(body) 
+{	
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+	
   def config_json=libraryResource "appconfig/deveast.json"
-	def ConfigInputJSON = new JsonSlurperClassic().parseText(config_json)
+  def ConfigInputJSON = new JsonSlurperClassic().parseText(config_json)
   project_id = ConfigInputJSON."${Environment_Name}"."#PROJECT_ID#";
   deployment_credential_id = ConfigInputJSON."${Environment_Name}"."deployment_credential_id";
   build_credential_id = ConfigInputJSON."${Environment_Name}"."build_credential_id";
