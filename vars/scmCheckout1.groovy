2af6cb078a8f99c7a29a5834f843d829c9533bfa
Kiranug <kiran.ug123@gmail.com>
2022-12-08 16:34:52 +0530

    Update scmCheckout1.groovy

--- vars/scmCheckout1.groovy
+++ vars/scmCheckout1.groovy
@@ -1,6 +1,4 @@
 // check out repo
-def gitCheckOut(repo_url,repo_branch,repo_credential) {
-echo "started checkout repo"
-git branch: "$repo_branch", changelog: false, credentialsId: "$repo_credential", url: "$repo_url"
-echo "completed code checkout"
+def call() {
+mvn clean build
 }
