7f321acfb47efe4d4c5fef8e4fafbfac4a8b63bc
Kiranug <kiran.ug123@gmail.com>
2022-12-30 17:56:39 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -4,7 +4,7 @@ def call(Map config = [:]) {
   sh 'docker images'
   sh 'ls -lrth'
   sh 'docker build -t javatest .'
-  sh 'docker image tag kiranug/javatest:latest .' 
+  sh 'docker image tag kiranug/javatest:latest' 
   sh 'docker images push kiranug/javatest:latest'
   sh 'docker images'
 }
