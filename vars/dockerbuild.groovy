7639c6b6ec631237d118cab608a44963be4d385e
Kiranug <kiran.ug123@gmail.com>
2023-02-27 12:13:08 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -3,7 +3,8 @@
 def call(Map config = [:]) {
   sh 'docker images'
   sh 'ls -lrth'
-  sh 'docker build -t "${REGISTRY}\/${IMAGE}:${BUILD_NUMBER}" .'
-  sh 'docker push "${REGISTRY}\/${IMAGE}:${BUILD_NUMBER}"'
-  sh 'docker images'
+  sh 'az acr build --registry $REGISTRY --image ${IMAGE}:${BUILD_NUMBER} .'
+//  sh 'docker build -t "${REGISTRY}/${IMAGE}:${BUILD_NUMBER}" .'
+//  sh 'docker push "${REGISTRY}/${IMAGE}:${BUILD_NUMBER}"'
+   sh 'docker images'
 }
