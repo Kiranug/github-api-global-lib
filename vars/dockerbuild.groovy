1c8422298c188c18844273644ba4abba7e3e2cc6
Kiranug <kiran.ug123@gmail.com>
2023-02-27 12:08:29 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -3,7 +3,7 @@
 def call(Map config = [:]) {
   sh 'docker images'
   sh 'ls -lrth'
-  sh 'docker build -t "${REGISTRY}/${IMAGE}:${BUILD_NUMBER}" .'
-  sh 'docker push "${REGISTRY}/${IMAGE}:${BUILD_NUMBER}"'
+  sh 'docker build -t "${REGISTRY}\/${IMAGE}:${BUILD_NUMBER}" .'
+  sh 'docker push "${REGISTRY}\/${IMAGE}:${BUILD_NUMBER}"'
   sh 'docker images'
 }
